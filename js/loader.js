8f885a74767f8c91f4b40931f758f65e650f801e
Jared Blumer <jaredblumer@Jared-Blumer.local>
2017-10-23 19:52:33 -0400

    Added loading page

---
+++ js/loader.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+  //Loading Page
+  var pageLoader = function() {
+
+    var $elements = $('body').find('img[src]');
+    $('body [style]').each(function(){
+      var src = $(this).css('background-image').replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+        if(src) {
+            $elements = $elements.add($('<img src="' + src + '"/>'));
+          }
+     });
+
+     var $loading = $('#loader');
+     var $loadPercentageLine = $('#loader-line');
+     var $loadPercentageText = $('#loader-percentage');
+     var elementsLoaded = 0;
+     var speed = 5000;
+
+     var animatePercentage = function(e) {
+       console.log(e);
+       $loadPercentageText.text(parseInt(e));
+
+     };
+
+    var loading = function() {
+      var percentage = 0;
+      if ($elements.length) {
+        percentage = parseInt((elementsLoaded / $elements.length) * 100);
+      }
+
+      $loadPercentageLine.stop().animate({
+        height:percentage + '%'
+      }, speed);
+
+      $loadPercentageText.stop().animate({
+            percentage:percentage
+        }, {
+            duration: speed,
+            step: animatePercentage
+        });
+    };
+
+    if($elements.length) {
+      loading();
+
+      $elements.each(function() {
+        elementsLoaded++;
+        loading();
+      });
+    }
+  };
+
+  pageLoader();
+
+});
