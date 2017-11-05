c8053a8e6dcbcb1dfb25ce0e6f0d53f6999a7c9d
Jared Blumer <jaredblumer@Jared-Blumer.local>
2017-11-05 16:44:59 -0500

    Added Contact Page

--- js/menu.js
+++ js/menu.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       $("#hamburger").removeClass("rotate");
       $("#menu-box").removeClass("menu-box-open");
       $('.dropdown').animate({
-        top: '-180px'
+        top: '-300px'
       }, 400);
     } else {
       $("#hamburger").addClass('rotate');
@@ -18,6 +18,20 @@ $(document).ready(function() {
     }
   });
 
+  // About Pop-Up
+
+  $('#contactLink').click(function() {
+    $('.dropdown').hide();
+    $('#container').hide();
+    $('#contact').css({'display': 'block'});
+  });
+
+  $('#contactExit').click(function() {
+    $('.dropdown').show();
+    $('#contact').css({'display': 'none'});
+    $('#container').show();
+  });
+
   // Blog Category Dropdown Menu
 
   $(".header-dropdown").click(function() {
@@ -26,6 +40,41 @@ $(document).ready(function() {
     } else {
       $(".categories").slideUp();
     }
+  });
+
+  var activeCategory = 'all';
+
+  var blogFilter = function(category) {
+    var categoryClass = '.' + category;
+    $('.selected').text(category);
+    $(".categories").slideUp();
+    if (category === 'all' && activeCategory === 'all') {
+      return;
+    }
+    else if (category === 'all' && activeCategory !== 'all') {
+      $('.blog-container').hide();
+      $('.blog-topic').hide();
+      $('.angular').show();
+      $('.css').show();
+      $('.express').show();
+      $('.javascript').show();
+      $('.mongodb').show();
+      $('.node').show();
+      $('.blog-container').slideDown();
+      activeCategory = 'all';
+    }
+    else {
+      $('.blog-container').hide();
+      $('.blog-topic').hide();
+      $(categoryClass).show();
+      $('.blog-container').slideDown();
+      activeCategory = category;
+    }
+  };
+
+  $('.categorySelection').click(function() {
+    var category = $(this).attr('id');
+    blogFilter(category);
   });
 
 });
